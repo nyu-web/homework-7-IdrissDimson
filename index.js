9e43b023c23f8d2a71dda4d71d8894130c492914
Idriss Dimson <idriss.dimson@gmail.com>
2018-10-22 13:01:50 -0400

    finished game logic

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const playerOne = "x";
-const playerTwo = "o";
+let playBoard = document.createElement("div");
+let playerOne = true;
 
-function onGridClick() {
+function playGame() {
 	// body...
-	
+	if (event.target.textContent === "") {
+		if (playerOne === true) {
+			event.target.textContent = "X";
+		} else {
+			event.target.textContent = "O";
+		}
+		playerOne = !playerOne;
+	} else {
+		console.log("Someone marked this spot!");
+	}
 }
+
+function createPlayBoard() {
+	for (var i = 0; i < 9; i++) {
+		playBoard = document.createElement("div");
+		document.getElementById("gameBoard").appendChild(playBoard);
+	}
+	document.getElementById("gameBoard").addEventListener('click', playGame);
+}
+
+createPlayBoard();
